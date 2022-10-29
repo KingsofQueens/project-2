a6fc68b448ff9b3e688535aa520e9ac87ffe2c59
joao <njoao99@gmail.com>
2022-10-29 15:29:56 +0100

    event route complete

--- routes/events.js
+++ routes/events.js
@@ -1,20 +1,101 @@
 'use strict';
 
 const express = require('express');
+const eventsRouter = express.Router();
 const routeGuardMiddleware = require('../middleware/route-guard');
-const User = require('../models/user');
 const Event = require('../models/event');
-const Follow = require('./../models/follow');
 const upload = require('./upload');
 
-const eventsRouter = express.Router();
-
-eventsRouter.get('/events', (req, res, next) => {
+eventsRouter.get('/events', routeGuardMiddleware, (req, res, next) => {
   res.render('events-create-edit/events');
 });
 
-eventsRouter.get('/events/create', (req, res, next) => {
-  res.render('events-create-edit/create');
+eventsRouter.post(
+  '/events/create',
+  routeGuardMiddleware,
+  upload.single('picture'),
+  (req, res, next) => {
+    const { title } = req.body;
+    const { description } = req.body;
+    const { path } = req.file;
+    let picture;
+    if (req.file) {
+      picture = req.file.path;
+    }
+    const { location } = req.body;
+    const { category } = req.body;
+    const { price } = req.body;
+    Event.create({
+      title,
+      description,
+      host: req.user._id,
+      picture,
+      location,
+      category,
+      price
+    })
+      .then(() => res.redirect('/home'))
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
+
+eventsRouter.get('/events/:id/edit', routeGuardMiddleware, (req, res, next) => {
+  const { id } = req.params;
+  Event.findById(id)
+    .then((event) => {
+      res.render('events-create-edit/edit', { event });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
+
+eventsRouter.post(
+  '/events/:id/edit',
+  routeGuardMiddleware,
+  upload.single('picture'),
+  (req, res, next) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    const { description } = req.body;
+    const { path } = req.file;
+    let picture;
+    if (req.file) {
+      picture = req.file.path;
+    }
+    const { location } = req.body;
+    const { category } = req.body;
+    const { price } = req.body;
+    Event.findByIdAndUpdate(id, {
+      title,
+      description,
+      picture,
+      location,
+      category,
+      price
+    })
+      .then((event) => {
+        res.redirect('/home');
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
+
+eventsRouter.post(
+  '/events/:id/delete',
+  routeGuardMiddleware,
+  (req, res, next) => {
+    const { id } = req.params;
+    Event.findByIdAndDelete(id)
+      .then(() => {
+        res.redirect('/home');
+      })
+      .catch((error) => next(error));
+  }
+);
 
 module.exports = eventsRouter;
